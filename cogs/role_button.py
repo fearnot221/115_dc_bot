8bd74b2ef12d794e25a02126c4e85dbf5756c537
fearnot221 <kenin221@gmail.com>
2025-03-29 22:28:52 +0800

    Add role_button_view

--- cogs/role_button.py
+++ cogs/role_button.py
@@ -1,7 +1,7 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
-from utils.role_button_ui import Main
+from utils.role_button_ui import Gay, Crown
 from bot import is_admin
 import asyncio
 import colorsys
@@ -12,29 +12,49 @@ class Role_Button(commands.Cog):
         self.bot = bot
         self.emoji = self.bot.emoji
     
+    role_choices = [
+        app_commands.Choice(name="Gay", value="gay"),
+        app_commands.Choice(name="Crown", value="crown"),
+    ]
+    
     @app_commands.command(name="role_button", description="建立領取身份組按鈕")
+    @app_commands.choices(category=role_choices)
     @is_admin()
-    async def setup_buttons(self, interaction: discord.Interaction):
+    async def setup_buttonss(self, interaction: discord.Interaction, category: str):
+        """根據選擇的類別建立身份組按鈕"""
+        
+        # 根據 category 設定不同的 view 和標題
+        if category == "gay":
+            embed_title = f"{self.emoji.get('gay1')} 多元身份認同"
+            embed_description = f"我們支持多元身份認同\n\n歡迎 {self.emoji.get('gay2')}"
+            view = Gay(bot=self.bot)
+        elif category == "crown":
+            embed_title = f"{self.emoji.get('gay1')} 多元身份認同"
+            embed_description = f"我們支持多元身份認同\n\n歡迎 {self.emoji.get('crown1')}"
+            view = Crown(bot=self.bot)
+        else:
+            await interaction.response.send_message("無效的選項！", ephemeral=True)
+            return
+
         embed = discord.Embed(
-            title=f"{self.emoji.get('gay1')} 多元身份認同",
-            description=f"我們支持多元身份認同\n\n歡迎 {self.emoji.get('gay2')}",
+            title=embed_title,
+            description=embed_description,
             color=0xFF0000
         )
         
-        view = Main(bot=self.bot)
         await interaction.response.send_message(embed=embed, view=view)
-            
+
+        # 彩虹漸變效果
         while True:
             for h in range(0, 361, 5):  # 色相 H 從 0° 變到 360°
                 r, g, b = colorsys.hsv_to_rgb(h / 360, 1, 1)  # 轉 RGB
                 hex_color = (int(r * 255) << 16) + (int(g * 255) << 8) + int(b * 255)
                 await interaction.edit_original_response(embed=discord.Embed(
-                    title=f"{self.emoji.get('gay1')} 多元身份認同",
-                    description=f"我們支持多元身份認同\n\n歡迎 {self.emoji.get('gay2')}",
+                    title=embed_title,
+                    description=embed_description,
                     color=hex_color
                 ))
                 await asyncio.sleep(0.1)  # 調整變色速度
-
 
 async def setup(bot):
     await bot.add_cog(Role_Button(bot))
--- utils/role_button_ui.py
+++ utils/role_button_ui.py
@@ -5,7 +5,7 @@ import asyncio
 import colorsys
 
 # 領取身份組按鈕面板
-class Main(View):
+class Gay(View):
     def __init__(self, bot=None):
         super().__init__(timeout=None)
         self.bot = bot
@@ -15,7 +15,7 @@ class Main(View):
             label="多元身份認同", 
             style=discord.ButtonStyle.primary,
             emoji=self.emoji.get('gay3'),
-            custom_id="role_button"
+            custom_id="role_button_gay"
         )
         button.callback = self.button_callback
         
@@ -32,32 +32,130 @@ class Main(View):
         role = interaction.guild.get_role(role_id)
         
         if role:
-            await interaction.user.add_roles(role)
+            if not role in interaction.user.roles:
+                
+                await interaction.user.add_roles(role)
             
-            embed = discord.Embed(
-                title="認同成功",
-                description=f"恭喜 {self.emoji.get('gay4')}",
-                color=0xFF0000
-            )
+                embed = discord.Embed(
+                    title="認同成功",
+                    description=f"恭喜 {self.emoji.get('gay4')}",
+                    color=0xFF0000
+                )
 
-            msg = await interaction.followup.send(embed=embed, ephemeral=True)
+                msg = await interaction.followup.send(embed=embed, ephemeral=True)
+                
+                while True:
+                    for h in range(0, 361, 5):  # 色相 H 從 0° 變到 360°
+                        r, g, b = colorsys.hsv_to_rgb(h / 360, 1, 1)  # 轉 RGB
+                        hex_color = (int(r * 255) << 16) + (int(g * 255) << 8) + int(b * 255)
+                        await msg.edit(embed=discord.Embed(
+                            title="認同成功",
+                            description=f"恭喜 {self.emoji.get('gay4')}",
+                            color=hex_color
+                        ))
+                        await asyncio.sleep(0.1)  # 調整變色速度
+            else:
+                
+                await interaction.user.remove_roles(role)
             
-            while True:
-                for h in range(0, 361, 5):  # 色相 H 從 0° 變到 360°
-                    r, g, b = colorsys.hsv_to_rgb(h / 360, 1, 1)  # 轉 RGB
-                    hex_color = (int(r * 255) << 16) + (int(g * 255) << 8) + int(b * 255)
-                    await msg.edit(embed=discord.Embed(
-                        title="認同成功",
-                        description=f"恭喜 {self.emoji.get('gay4')}",
-                        color=hex_color
-                    ))
-                    await asyncio.sleep(0.1)  # 調整變色速度
+                embed = discord.Embed(
+                    title="嗚嗚嗚已不認同",
+                    description=f"為什麼不認同 TT {self.emoji.get('gay4')}",
+                    color=0xFF0000
+                )
+
+                msg = await interaction.followup.send(embed=embed, ephemeral=True)
+                
+                while True:
+                    for h in range(0, 361, 5):  # 色相 H 從 0° 變到 360°
+                        r, g, b = colorsys.hsv_to_rgb(h / 360, 1, 1)  # 轉 RGB
+                        hex_color = (int(r * 255) << 16) + (int(g * 255) << 8) + int(b * 255)
+                        await msg.edit(embed=discord.Embed(
+                            title="嗚嗚嗚已不認同",
+                            description=f"為什麼不認同 TT {self.emoji.get('gay4')}",
+                            color=hex_color
+                        ))
+                        await asyncio.sleep(0.1)  # 調整變色速度
+
+class Crown(View):
+    def __init__(self, bot=None):
+        super().__init__(timeout=None)
+        self.bot = bot
+        self.emoji = self.bot.emoji
+        
+        button = Button(
+            label="多元身份認同", 
+            style=discord.ButtonStyle.primary,
+            emoji=self.emoji.get('crown1'),
+            custom_id="role_button_crown"
+        )
+        button.callback = self.button_callback
+        
+        self.add_item(button)
+    
+    # 驗證身份功能
+    async def button_callback(self, interaction: discord.Interaction):
+        await interaction.response.defer(ephemeral=True)
+        
+        db_manager = DatabaseManager(interaction.guild.id, interaction.guild.name)
+        
+        role_id = await db_manager.get_role_id_config(role_name="crown")
+
+        role = interaction.guild.get_role(role_id)
+        
+        if role:
+            if not role in interaction.user.roles:
+                
+                await interaction.user.add_roles(role)
+            
+                embed = discord.Embed(
+                    title="認同成功",
+                    description=f"恭喜 {self.emoji.get('crown1')}",
+                    color=0xFF0000
+                )
+
+                msg = await interaction.followup.send(embed=embed, ephemeral=True)
+                
+                while True:
+                    for h in range(0, 361, 5):  # 色相 H 從 0° 變到 360°
+                        r, g, b = colorsys.hsv_to_rgb(h / 360, 1, 1)  # 轉 RGB
+                        hex_color = (int(r * 255) << 16) + (int(g * 255) << 8) + int(b * 255)
+                        await msg.edit(embed=discord.Embed(
+                            title="認同成功",
+                            description=f"恭喜 {self.emoji.get('crown1')}",
+                            color=hex_color
+                        ))
+                        await asyncio.sleep(0.1)  # 調整變色速度
+            else:
+                
+                await interaction.user.remove_roles(role)
+            
+                embed = discord.Embed(
+                    title="嗚嗚嗚已不認同",
+                    description=f"為什麼不認同 TT {self.emoji.get('crown1')}",
+                    color=0xFF0000
+                )
+
+                msg = await interaction.followup.send(embed=embed, ephemeral=True)
+                
+                while True:
+                    for h in range(0, 361, 5):  # 色相 H 從 0° 變到 360°
+                        r, g, b = colorsys.hsv_to_rgb(h / 360, 1, 1)  # 轉 RGB
+                        hex_color = (int(r * 255) << 16) + (int(g * 255) << 8) + int(b * 255)
+                        await msg.edit(embed=discord.Embed(
+                            title="嗚嗚嗚已不認同",
+                            description=f"為什麼不認同 TT {self.emoji.get('crown1')}",
+                            color=hex_color
+                        ))
+                        await asyncio.sleep(0.1)  # 調整變色速度
 
 # 初始化函數，用於重啟 bot 時註冊所有持久化視圖
 def setup_persistent_views_role_button(bot):
     try:
         
-        bot.add_view(Main(bot=bot))
+        bot.add_view(Gay(bot=bot))
+        
+        bot.add_view(Crown(bot=bot))
         
         return True
     except Exception as e:
